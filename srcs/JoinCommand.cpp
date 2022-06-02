25a9b30e40bb99663d6956a375a1480d20afdfa0
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-02 17:06:26 +0200

    d

--- srcs/JoinCommand.cpp
+++ srcs/JoinCommand.cpp
@@ -6,7 +6,7 @@ JoinCommand::~JoinCommand() {}
 
 void JoinCommand::execute(Client *client, std::vector<std::string> arguments) {
 	(void)arguments;
-	//client->setUsername(arguments[0]);
-	//client->setRealName(arguments[1]);
+	//client->setUsername(arguments[1]);
+	//client->setRealName(arguments[2]);
 	client->welcome();
 }
