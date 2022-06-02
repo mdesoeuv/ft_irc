b59c1e142dbe0cf94b3c7046e3e4e643fee11b93
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-02 12:20:14 +0200

    rm ft irc .hpp

--- inc/ft_irc.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <string>
-
-#include <netinet/in.h> /* pour struct sockaddr_in */
-#include <arpa/inet.h> /* pour htons et inet_aton */
-#include <sys/socket.h>
