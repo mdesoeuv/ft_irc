f2d7e37a7e0ae1a72d436ab6e0345186b1c1790b
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-03 09:45:28 +0200

    added all commands

--- srcs/CommandHandler.cpp
+++ srcs/CommandHandler.cpp
@@ -5,8 +5,20 @@
 
 CommandHandler::CommandHandler(Server *server) : _server(server)
 {
+	_commands["PASS"] = new PassCommand(_server);
+	_commands["NICK"] = new NickCommand(_server);
 	_commands["USER"] = new UserCommand(_server);
+	_commands["QUIT"] = new QuitCommand(_server);
+
+	_commands["PING"] = new PingCommand(_server);
+	_commands["PONG"] = new PongCommand(_server);
 	_commands["JOIN"] = new JoinCommand(_server);
+	_commands["MODE"] = new ModeCommand(_server);
+	_commands["PART"] = new PartCommand(_server);
+	_commands["KICK"] = new KickCommand(_server);
+
+	_commands["PRIVMSG"] = new PrivMsgCommand(_server);
+	_commands["NOTICE"] = new NoticeCommand(_server);
 }
 
 CommandHandler::~CommandHandler()
