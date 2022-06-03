d33e7d4393e30ce993b2e90d085bfed7604c0e18
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-06-03 10:59:51 +0200

    welcome message define added back in utils.hpp

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-//Error Messages
-#define ERR_NONICKNAMEGIVEN(client)						"431 " + client + " :Nickname not given"
-#define ERR_NICKNAMEALREADYUSED(client)						"433 " + client + " " + client  + " :Nickname is already in use"
-#define ERR_CMDNEEDMOREPARAMS(client, command)				"461 " + client + " " + command + " :Not enough parameters"
+/* Welcome Message */
+
+#define RPL_WELCOME(source)							"001 " + source + " :Welcome " + source + " to the ft_irc network"
+
+/* Error Messages */
+
+#define ERR_NONICKNAMEGIVEN(client)					"431 " + client + " :Nickname not given"
+#define ERR_NICKNAMEALREADYUSED(client)				"433 " + client + " " + client  + " :Nickname is already in use"
+#define ERR_CMDNEEDMOREPARAMS(client, command)		"461 " + client + " " + command + " :Not enough parameters"
