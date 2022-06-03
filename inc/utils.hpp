2c0735c6441aa3ec3a83b42e6f5efcefb97afa9b
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-03 12:09:22 +0200

    rm vector

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -3,10 +3,10 @@
 /* replies Message */
 
 #define RPL_WELCOME(source)							"001 " + source + " :Welcome " + source + " to the ft_irc network"
-#define RPL_QUIT(client, message)					":" + client + " QUIT :Quit: " + message
+#define RPL_QUIT(myclient, message)					":" + myclient + " QUIT :Quit: " + message
 
 /* Error Messages */
 
-#define ERR_NONICKNAMEGIVEN(client)					"431 " + client + " :Nickname not given"
-#define ERR_NICKNAMEALREADYUSED(client)				"433 " + client + " " + client  + " :Nickname is already in use"
-#define ERR_CMDNEEDMOREPARAMS(client, command)		"461 " + client + " " + command + " :Not enough parameters"
+#define ERR_NONICKNAMEGIVEN(myclient)					"431 " + myclient + " :Nickname not given"
+#define ERR_NICKNAMEALREADYUSED(myclient)				"433 " + myclient + " " + myclient  + " :Nickname is already in use"
+#define ERR_CMDNEEDMOREPARAMS(myclient, command)		"461 " + myclient + " " + command + "  :Not enough parameters"
