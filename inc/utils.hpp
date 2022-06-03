2abf964d67fad6284929b39e80b6000d17d3d34d
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-03 09:49:56 +0200

    added utils

---
+++ inc/utils.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#define RPL_WELCOME(source)						"001 " + source + " :Welcome " + source + " to the ft_irc network"
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,5 +1,4 @@
 #include "../inc/Client.hpp"
-#define RPL_WELCOME(source)						"001 " + source + " :Welcome " + source + " to the ft_irc network"
 
 Client::Client( int fd, 
 				const std::string& nick, 
