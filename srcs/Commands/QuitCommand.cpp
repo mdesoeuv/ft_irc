5982a16b0d610d7d2a5c59bc4391c511f30105f0
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-03 13:53:11 +0200

    added debug

--- srcs/Commands/QuitCommand.cpp
+++ srcs/Commands/QuitCommand.cpp
@@ -8,4 +8,5 @@ void QuitCommand::execute(Client& client, std::string arguments) {
 
 	std::string reason = arguments.empty() ? "Quit for unknown reason !" : arguments;
 	client.write(RPL_QUIT(client.getPrefix(), reason));
+	std::cout <<"Client has Quit " << client.getNickname() << std::endl;
 }
