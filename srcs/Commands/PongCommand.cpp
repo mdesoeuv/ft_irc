09c5f468ed7e228be6b9fd3d3933259d4eb35329
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-09 14:25:15 +0200

    changed pong

--- srcs/Commands/PongCommand.cpp
+++ srcs/Commands/PongCommand.cpp
@@ -10,7 +10,5 @@ void PongCommand::execute(Client& client, std::string arguments) {
 		client.reply(ERR_CMDNEEDMOREPARAMS(client.getNickname(), "PONG"));
 		return;
 	}
-
-	client.write(RPL_PONG(client.getPrefix(), arguments));
 	std::cout <<"Client has Pong " << client.getNickname() << std::endl;
 }
