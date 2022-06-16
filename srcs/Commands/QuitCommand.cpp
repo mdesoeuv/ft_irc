cbf4f120af0ff55194811ce8221b706a7207b926
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-06-16 16:11:14 +0200

    pollerr if no client fix

--- srcs/Commands/QuitCommand.cpp
+++ srcs/Commands/QuitCommand.cpp
@@ -5,10 +5,8 @@ QuitCommand::QuitCommand(Server *server) : Command(server) {}
 QuitCommand::~QuitCommand() {}
 
 void QuitCommand::execute(Client& client, std::string arguments) {
+	(void)client;
 	arguments = arguments.substr(1, arguments.size());
 	std::string reason = arguments.empty() ? "Quit for unknown reason !" : arguments;
-	_server->allChannelLeave(client, RPL_QUIT(client.getPrefix(), reason));
-	client.write(RPL_QUIT(client.getPrefix(), reason));
-	_server->addClientToDelete(client.getSocketfd());
 	std::cout << "Client has Quit " << std::endl;
 }
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -42,7 +42,8 @@ void Server::start()
 			
 		// poll est une fonction qui boucle jusqu'à l'arrivée de nouvelles data
 		if (poll(_pollfds.begin().base(), _pollfds.size(), -1) < 0)
-			throw std::runtime_error("Error while polling from fd.");
+			if (g_ServerRunning)
+				throw std::runtime_error("Error while polling from fd.");
 		//  Un des fd a un nouveau message, on les parcourt pour savoir lequel
 		for (pollfds_iterator it = _pollfds.begin(); it != _pollfds.end(); ++it)
 		{
