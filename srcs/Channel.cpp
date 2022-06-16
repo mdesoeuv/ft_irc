711d91fed6330413c9f2b1553f87a029cf823d21
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-06-16 17:30:19 +0200

    d

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -256,8 +256,9 @@ void Channel::removeExceptionBan(const std::string nickname)
 	Client to_kick_user;
 	if (isUser(nickname) && isBanned(nickname))
 	{
-		// TODO: reply kick 
-
+		kicked_user = getChanClient(nickname);
+		std::string	prefix = isMode('a') ? "anonymous!anonymous@anonymous." : kicked_user.getPrefix();
+		broadcastMessage(":" + prefix + " KICK " + kicked_user.getNickname());
 		to_kick_user = getChanClient(nickname);
 		delUser(to_kick_user);
 		_user_nb--;
