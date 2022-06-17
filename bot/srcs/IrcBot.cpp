246d79625e1269c68d09b92baa2fbdd1137cf6b4
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-06-17 10:57:33 +0200

    Bot rework with poll() and _sendQueue

--- bot/srcs/IrcBot.cpp
+++ bot/srcs/IrcBot.cpp
@@ -1,7 +1,7 @@
 #include "../inc/IrcBot.hpp"
 
 IrcBot::IrcBot(const std::string &host, const std::string &port, const std::string &password) :
-		_host(host), _port(port), _password(password) {
+		_host(host), _port(port), _password(password), _serverPrefix("ft_irc@127.0.0.1") {
 	_sock = newSocket();
 }
 
@@ -37,39 +37,71 @@ int IrcBot::newSocket() {
 }
 
 void IrcBot::start() {
+
+	bool g_BotRunning = true;
+	pollfd bot_fd = {_sock, POLLIN | POLLOUT, 0};
+	_pollfds.push_back(bot_fd);
 	authenticate("BotDePaille");
-	while (1)
+	while (g_BotRunning)
 	{
-//Listen for incoming messages from server
-	char buffer[256];
-	int length;
+		// poll loop
+		if (poll(_pollfds.begin().base(), _pollfds.size(), -1) < 0)
+			if (g_BotRunning)
+				throw std::runtime_error("Error while polling from fd.");
+		pollfds_iterator it = _pollfds.begin();
+		if (it->revents & POLLHUP)
+		{
+			g_BotRunning = false; // server disconnect -> shutdown du bot
+			break;
+		}
 
-	while ((length = recv(_sock, buffer, 256, 0)) > 0) {
-		buffer[length] = 0;
-		onMessageReceived(std::string(buffer, length));
+		if (it->revents & POLLIN) // POLLIN: le bot reçoit un message
+		{
+			onServerMessage(it->fd);
+		}
+
+		// POLLOUT: le bot peut send
+		if (it->revents & POLLOUT)
+		{
+			sendMessageToServer(_clients[it->fd]);
+		}
+
+		//server POLLERR -> g_BotRunning = false
+		if (it->revents & POLLERR)
+		{
+			g_BotRunning = false;
+		}
 	}
-	}
+	// addSendQueue("QUIT :Bye !");
 	close(_sock);
-	sendMessageToServer("QUIT :Bye !");
-
 }
 
-void IrcBot::sendMessageToServer(const std::string &message) {
 
-	std::cout << "Sending Message : " << message << std::endl;
 
-	std::string terminatedString = message + "\r\n";
-	send(_sock, terminatedString.c_str(), terminatedString.size(), 0);
+void IrcBot::addSendQueue(const std::string& message) {
+	
+	std::string full_message = message;
+	if (full_message.size() > MSG_SIZE_LIMIT)
+		full_message.resize(MSG_SIZE_LIMIT - 2);
+	full_message += "\r\n";
+	_sendQueue += full_message;
 }
+
+void IrcBot::sendMessageToServer()
+{
+
+	if (_sendQueue.empty())
+		return;
+	int sent_bytes = send(_sock, _sendQueue.c_str(), _sendQueue.length(), 0);
+	if (sent_bytes < 0)
+		std::cout << "Error while sending message to server." << std::endl;
+	std::cout << "message sent :" + _sendQueue.substr(0, sent_bytes) << std::endl;
+	_sendQueue.erase(0, sent_bytes);
+}
+
 
 void IrcBot::sendPrivMsg(const std::string &source, const std::string &message) {
-	sendMessageToServer("PRIVMSG " + source + " :" + message);
-}
-
-void IrcBot::authenticate(const std::string &nickname) {
-	sendMessageToServer("PASS " + _password);
-	sendMessageToServer("NICK " + nickname);
-	sendMessageToServer("USER botDePaille 0 * :Custom ft_irc's bot");
+	addSendQueue("PRIVMSG " + source + " :" + message);
 }
 
 void IrcBot::onMessageReceived(const std::string &message) {
@@ -84,4 +116,60 @@ void IrcBot::onMessageReceived(const std::string &message) {
 	std::string type = splited_args.at(1);
 
 	//replyToCommand(source, type, std::vector<std::string>(splited_args.begin() + 2, splited_args.end())); TODO
+}
+
+std::string	IrcBot::extractMessage() {
+
+	std::string message; 
+
+	size_t pos = _messageBuffer.find("\r\n");
+	message = _messageBuffer.substr(0, pos);
+	_messageBuffer.erase(0, pos + 2);
+
+	std::cout << "extracted message :" + message << std::endl;
+	return message;
+}
+
+void IrcBot::onServerMessage(int fd)
+{
+	int read_bytes = -10;
+	char buffer[BUFFER_SIZE + 1];
+
+	bzero(buffer, BUFFER_SIZE + 1);
+	while (read_bytes != 0)
+	{
+		bzero(buffer, BUFFER_SIZE);
+		read_bytes = recv(fd, buffer, BUFFER_SIZE, 0);
+		if (read_bytes < 0)
+			break;
+		std::cout << "bytes read :" << read_bytes << std::endl;
+		buffer[read_bytes] = '\0';
+		std::cout << "packet received :" + std::string(buffer) + "//" << std::endl;
+		_messageBuffer.append(buffer);
+		while (_messageBuffer.find("\r\n") < _messageBuffer.size())
+		{
+			parseExecute(_messageBuffer.extractMessage());
+		}
+	}
+}
+
+void IrcBot::authenticate(const std::string &nickname) {
+	addSendQueue("PASS " + _password);
+	addSendQueue("NICK " + nickname);
+	addSendQueue("USER botDePaille 0 * :Custom ft_irc's bot");
+}
+
+std::string	IrcBot::ParseSender(const std::string& message) {
+	size_t pos = message.find(" ");
+	std::string sender = message.substr(1, pos);
+
+	return sender;
+}
+
+void IrcBot::parseExecute(const std::string& message) {
+
+	std::string sender = ParseSender(message);
+	std::string response = "Coucou c'est moi le bot de paille !";
+	if (sender != _serverPrefix)
+		sendPrivMsg(sender, response);
 }
